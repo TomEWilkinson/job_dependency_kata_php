2b67c58947445cfac0e80eb784e9b49efe43ab62
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 19:19:48 +0100

    deal with multiple jobs with a single dependency

--- JobDependencyKata.php
+++ JobDependencyKata.php
@@ -7,16 +7,26 @@
  * @param string $job
  * @return void
  */
-function orderList($jobs) {
+function orderList($jobs)
+{
 
-    if(!$jobs)
-    {
+    if (!$jobs) {
         return "";
     }
 
     $orderedList = [];
+    $dependencyList = [];
     foreach ($jobs as $job => $dependency) {
-       array_push($orderedList, $job);
+
+        if ($dependency) {
+            $dependencyList[$dependency] = $job;
+        }
+
+        if (isset($dependencyList[$job])) {
+            array_splice($orderedList, array_search($dependencyList[$job], $orderedList), 0, $job);
+            continue;
+        }
+        array_push($orderedList, $job);
     }
 
     return $orderedList;
