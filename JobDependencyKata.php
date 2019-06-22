b70be4155f787caa774c06ee8921532280e079c8
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 20:22:56 +0100

    added check for the circle dependency

--- JobDependencyKata.php
+++ JobDependencyKata.php
@@ -28,10 +28,35 @@ function orderList($jobs)
 
         if (isset($dependencyList[$job])) {
             array_splice($orderedList, array_search($dependencyList[$job], $orderedList), 0, $job);
+            circleDependencyCheck($dependencyList, $dependencyList[$job], $dependency);
             continue;
         }
         array_push($orderedList, $job);
     }
 
     return $orderedList;
+}
+
+/**
+ * a recucsive function to check for a circle dependency
+ *
+ * @param array $dependencyList
+ * @param string $nextDependency
+ * @param string $originalDependency
+ * @return bool
+ */
+function circleDependencyCheck($dependencyList, $nextDependency, $originalDependency)
+{
+
+    //if the orginal depency equals the next dependency we have a circle
+    if (isset($dependencyList[$nextDependency]) && $originalDependency ==  $dependencyList[$nextDependency]) {
+        throw new Exception('Jobs Cannot cause a cirlce depdency', 101);
+    }
+
+    //if it's not undefined the chain of dependencies continues
+    if (isset($dependencyList[$nextDependency])) {
+        circleDependencyCheck($dependencyList, $dependencyList[$nextDependency], $originalDependency);
+    } else {
+        return false;
+    }
 }
