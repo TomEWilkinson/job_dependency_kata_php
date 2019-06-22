0fef407540b1905f1479378ead530cd3bff78d6c
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 17:51:25 +0100

    added comments to tests

--- JobDependencyKataTests.php
+++ JobDependencyKataTests.php
@@ -6,13 +6,18 @@ use PHPUnit\Framework\TestCase;
 
 final class JobDependencyKataTests extends TestCase
 {
-    /** @test */
+    /** @test 
+     * passes in an empty string to the function and expects an empty job list back
+     */
     public function TestEmptyString(): void
     {
         $this->assertSame("", orderList(""));
     }
 
-    /** @test */
+    /** @test 
+     * passing in a key value array containing the job and an empty dependency value
+     * expects array containing the job passed in
+     */
     public function TestSingleJob(): void
     {
         $jobs = array(
