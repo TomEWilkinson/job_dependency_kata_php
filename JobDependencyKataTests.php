6fd6a524059f6dc8dc4abf2bf4f2796f23669085
Tom Wilkinson <tom@Toms-MacBook-Pro.local>
2019-06-22 19:27:13 +0100

    added in tests for multiple jobs with mulitple dependencies

--- JobDependencyKataTests.php
+++ JobDependencyKataTests.php
@@ -47,7 +47,7 @@ final class JobDependencyKataTests extends TestCase
 
     /** @test 
      * passing in a key value array containing 3 jobs with one that has a dependency
-     * expects array containing the jobs passed in
+     * expects array containing the jobs passed oredered by depencies
      */
     public function TestMultipleJobsWithSingleDependency(): void
     {
@@ -59,5 +59,27 @@ final class JobDependencyKataTests extends TestCase
 
         $orderedList = orderList($jobs);
         $this->assertTrue(array_search('c', $orderedList) < array_search('b', $orderedList));
+    }
+
+    /** @test 
+     * passing in a key value array containing multiple jobs with multiple dependencies
+     * expects array containing the jobs passed ino redered by depencies
+     */
+    public function TestMultipleJobsWithMultipleDependencies(): void
+    {
+        $jobs = array(
+            "a" => "",
+            "b" => "c",
+            "c" => "f",
+            "d" => "a",
+            "e" => "b",
+            "f" => ""
+        );
+
+        $orderedList = orderList($jobs);
+        $this->assertTrue(array_search('f', $orderedList) < array_search('c', $orderedList));
+        $this->assertTrue(array_search('c', $orderedList) < array_search('b', $orderedList));
+        $this->assertTrue(array_search('b', $orderedList) < array_search('e', $orderedList));
+        $this->assertTrue(array_search('a', $orderedList) < array_search('d', $orderedList));
     }
 }
